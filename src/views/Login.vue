94d93bf13653db493dff901d9e589cf4c5f0c161
tpc-ht <2212234496@qq.com>
2020-01-22 14:43:03 +0800

    这是单独文件上传

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,8 @@
 <template>
-  <!--<el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">-->
-    <!--<h3 class="title">系统登录</h3>-->
-    <!--<el-form-item prop="phone">-->
-      <!--<el-input type="text" v-model="ruleForm2.phone" auto-complete="off" placeholder="账号"></el-input>-->
-    <!--</el-form-item>-->
-    <!--<el-form-item prop="pwd">-->
-      <!--<el-input type="password" v-model="ruleForm2.pwd" auto-complete="off" placeholder="密码"></el-input>-->
-    <!--</el-form-item>-->
-    <!--<el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>-->
-    <!--<el-form-item style="width:100%;">-->
-      <!--<el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>-->
-    <!--</el-form-item>-->
-  <!--</el-form>-->
   <div class="login-container">
     <el-form ref="ruleForm2" :model="ruleForm2" :rules="rules2" class="login-form" auto-complete="on" label-position="left" v-show="loginShow">
       <div class="title-container">
-        <h3 class="title">登录</h3>
+        <h3 class="title">测试登录</h3>
       </div>
       <el-form-item prop="phone">
         <span class="svg-container">
